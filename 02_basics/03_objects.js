9d113c899d5a54d4d3265b0a43cc846740eb0a0e
Somo2905 <127102199+Somo2905@users.noreply.github.com>
2024-01-22 12:13:56 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -21,11 +21,11 @@ const JsUser = {
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
-
+console.log(typeof JsUser);
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+ console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
@@ -34,5 +34,5 @@ JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
+console.log(typeof (JsUser.greeting));
 console.log(JsUser.greetingTwo());
